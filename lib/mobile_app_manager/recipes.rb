abef43fad3f1a6ce3a6166f3029f21bb49fe4e75
Joseph Bell <jbell@numerex.com>
2012-03-20 18:13:36 -0500

    upload test

--- lib/mobile_app_manager/recipes.rb
+++ lib/mobile_app_manager/recipes.rb
@@ -5,7 +5,8 @@ Capistrano::Configuration.instance.load do
   namespace :mobile_app_manager do
     desc "Publish an IPA to a remote deployment server"
     task :publish do
-      puts "hey"
+      puts "Uploading IPA to #{deploy_to}"
+      upload("ipa", #{deploy_to}/public/mobileapps")
     end
   end
 
