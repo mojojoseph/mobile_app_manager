1b0e8ea641b8916d259ce07d5a1fad49b7eb941d
Joseph Bell <jbell@numerex.com>
2012-03-20 18:01:45 -0500

    remove mobile_app_include so engine doesn't get load

--- lib/mobile_app_manager/recipes.rb
+++ lib/mobile_app_manager/recipes.rb
@@ -1,12 +1,12 @@
-require 'mobile_app_manager'
+#require 'mobile_app_manager'
 
 Capistrano::Configuration.instance.load do
 
-namespace :mobile_app_manager do
-  desc "Publish an IPA to a remote deployment server"
-  task :publish do
-    puts "hey"
+  namespace :mobile_app_manager do
+    desc "Publish an IPA to a remote deployment server"
+    task :publish do
+      puts "hey"
+    end
   end
-end
 
 end
