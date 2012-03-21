3e7ce0003844c1afe01e30205be8d9733a792711
Joseph Bell <jbell@numerex.com>
2012-03-21 09:17:28 -0500

    Moving back to Rails 3.0.9

--- lib/mobile_app_manager/recipes.rb
+++ lib/mobile_app_manager/recipes.rb
@@ -1,13 +1,39 @@
-#require 'mobile_app_manager'
-
 Capistrano::Configuration.instance.load do
 
   namespace :mobile_app_manager do
+
+    desc "Symlink"
+    task :symlink_mobileapps do
+      run ("mkdir #{shared_path}/mobileapps")
+      run ("ln -s #{shared_path}/mobileapps #{current_path}/public/mobileapps")
+    end
+
+
     desc "Publish an IPA to a remote deployment server"
     task :publish do
-      puts "Uploading IPA to #{deploy_to}"
-      upload("ipa", #{deploy_to}/public/mobileapps")
+      puts "Uploading IPA to #{current_release}"
+      puts "#{shared_path}"
+
+      cap_root   = Pathname.getwd
+      build_root = cap_root.join "mobileapps"
+
+      configuration_file = build_root.join 'mobileapps.yml'
+      build_config       = YAML::load(File.open(configuration_file))
+      ios_build_dir      = build_root.join build_config['ios']['directory']
+
+      # Dirty way, just iterate over all of the files and pick the ones
+      # we want, namely the .ipa and .plist
+      ios_build_dir.each_child do |f|
+        if f.extname == ".ipa" then
+          fname = f.basename
+          upload(f.to_s, "#{shared_path}/mobileapps/#{fname}")
+        end
+        
+        if f.extname == ".plist" then
+          fname = f.basename
+          upload(f.to_s, "#{shared_path}/mobileapps/#{fname}")
+        end
+      end
     end
   end
-
 end
